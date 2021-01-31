ef5c55a9b7094de5d532c40aa665b0c95bfd077b
dengbin <dengbin@cmcm.com>
2021-01-31 22:02:22 +0800

    init

---
+++ Tools/Tools/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tools
+//
+//  Created by dengbin on 2021/1/31.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tools/Tools/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Tools
+//
+//  Created by dengbin on 2021/1/31.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
